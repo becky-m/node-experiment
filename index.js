c5c9084d9c43d08cb7e03fe1b04042b6755611f8
Beca-work <rebecca@kloodle.com>
2018-06-27 00:28:06 +0100

    change text

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express(); //a new application that represents an express app.
 //set up configuration that will listen to requests and route the HTTP requests to different handlers
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
-const PORT = process.env.PORT; //when our deployment server runs application it will set various environment runtime variables and configuration. This is the current port that we need to use to access the webserver.
+const PORT = process.env.PORT || 5000; //when our deployment server runs application it will set various environment runtime variables and configuration. This is the current port that we need to use to access the webserver.
 app.listen(PORT); //tell node to look at traffic coming in on port 5000
